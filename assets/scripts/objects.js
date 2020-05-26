3ac1fc4ed54f1853187d3f5027060cfe4eb52c92
Karl Warner <KarlWarner.dev@gmail.com>
2020-05-26 11:47:56 +0100

    first commit

---
+++ assets/scripts/objects.js
@@ -0,0 +1,40 @@
+// build app, work on form validation using HTML 5
+// https://developer.mozilla.org/en-US/docs/Learn/Forms/HTML5_input_types
+
+const btnAddMovie = document.getElementById('btn-add-movie');
+const btnSearch = document.getElementById('btn-search');
+const movieInputs = document
+  .getElementById('user-input')
+  .querySelectorAll('input');
+const searchInput = document.getElementById('filter-title');
+const movies = new Map();
+
+const validateMovieTitleInput = () => (movieInputs[0].value ? true : false);
+
+const validateSearchInput = () => (searchInput.value ? true : false);
+
+const addMovieHandler = () => {
+  if (validateMovieTitleInput()) {
+    const keyName = movieInputs[1].value || 'defaultKey';
+    const value = movieInputs[2].value || 'defaultValue';
+    movies.set(movieInputs[0].value, { [keyName]: value });
+  }
+};
+
+const searchMovieHandler = () => {
+  if (validateSearchInput()) {
+    const searchTerms = searchInput.value.split(' ');
+    const keyToSearchTerms = Array.from(movies.keys()).map((key) =>
+      key.toLowerCase().split(' ')
+    );
+    for (const key of searchTerms.toLowerCase()) {
+
+      movies.has(key)
+        ? console.log('has key')
+        : console.log("doesn't have key");
+    }
+  }
+};
+
+btnAddMovie.addEventListener('click', addMovieHandler);
+btnSearch.addEventListener('click', searchMovieHandler);

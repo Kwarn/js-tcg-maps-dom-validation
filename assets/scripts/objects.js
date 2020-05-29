705720b8df6f0caab7939de510fadd923909f672
Kwarn <karlwarner.dev@gmail.com>
2020-05-29 10:53:51 +0100

    Regex search implemented, lots of refactoring.

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -2,74 +2,95 @@
 // https://developer.mozilla.org/en-US/docs/Learn/Forms/HTML5_input_types
 
 // to-do:
-// Maps only contain unique values, add duplicate user input handling
-// add functionality to remove list elements and replace list with search results
+// toggle movie/search list functionality
+// bug - same search elements can appear twice, 
+// if both search terms are in the same title -consider array.from(new Set())
 
-const ul = document.getElementById('movie-list');
-const btnAddMovie = document.getElementById('btn-add-movie');
-const btnSearch = document.getElementById('btn-search');
+const movieList = document.getElementById("movie-list");
+const searchList = document.getElementById("search-list");
+const btnAddMovie = document.getElementById("btn-add-movie");
+const btnSearch = document.getElementById("btn-search");
 const movieInputs = document
-  .getElementById('user-input')
-  .querySelectorAll('input');
-const searchInput = document.getElementById('filter-title');
+	.getElementById("user-input")
+	.querySelectorAll("input");
+const searchInput = document.getElementById("filter-title");
 const movies = new Map();
 
-const showMovieList = () => {
-  ul.classList.add('visible');
+const showList = (targetElement) => targetElement.classList.add("visible");
+const hideList = (targetElement) => targetElement.classList.remove("visible");
+const toggleList = () => {};
+
+const clearSearchResults = () => {
+	while (searchList.lastChild) {
+		searchList.removeChild(searchList.lastChild);
+	}
 };
 
-const removeAllListElements = () => {
-  while (ul.firstChild) {
-    ul.removeChild(ul.firstChild);
-  }
-};
-
-const searchMovieKeys = (searchTerm) => {
-  Array.from(movies.keys()).filter((key) => {
-    if (key.toLowerCase().split().includes(searchTerm)) {
-      removeAllListElements();
-      createAppendListElements(key, Object.entries(movies.get(key)));
-    }
-  });
-};
-
-const startSearch = () => {
-  const searchTerms = searchInput.value.toLowerCase().split(' ');
-  for (const searchTerm of searchTerms) {
-    searchMovieKeys(searchTerms);
-  }
-};
-
-const createAppendListElements = (title, extraInfo) => {
-  const [extraInfoName, extraInfoValue] = extraInfo[0];
-  const li = document.createElement('li');
-  li.innerHTML = `
-    <div>
-      <h2>${title}</h2> 
-      <p>${extraInfoName}: ${extraInfoValue}</p>
-    </div>
+const createAppendListElements = (targetList, title, extraInfoArr) => {
+	const [extraInfoKey, extraInfoValue] = extraInfoArr;
+	const li = document.createElement("li");
+	li.innerHTML = `
+  <div>
+  <h2>${title}</h2> 
+  <p>${extraInfoKey}: ${extraInfoValue}</p>
+  </div>
   `;
-  ul.appendChild(li);
-  showMovieList();
+	targetList === movieList ? movieList.appendChild(li) : searchList.appendChild(li);
 };
 
-const validateMovieTitleInput = () => (movieInputs[0].value ? true : false);
-
-const validateSearchInput = () => (searchInput.value ? true : false);
-
-const addMovieHandler = () => {
-  if (validateMovieTitleInput()) {
-    const keyName = movieInputs[1].value || 'Extra Info';
-    const value = movieInputs[2].value || 'None!';
-    movies.set(movieInputs[0].value, { [keyName]: value });
-  }
+const prepSearchElements = (titleMatches) => {
+	for (const title of titleMatches) {
+		createAppendListElements(
+			searchList,
+			title,
+			Object.entries(movies.get(title)).flat()
+		);
+	}
 };
 
-const searchMovieHandler = () => {
-  if (validateSearchInput()) {
-    startSearch();
-  }
+// uses Regex to search map.keys() (movie titles) for exact search term
+const searchMovieTitles = (searchTerms) => {
+	const titleMatches = [];
+	for (const searchTerm of searchTerms)
+		titleMatches.push(
+			Array.from(movies.keys()).filter((key) =>
+				key.match(new RegExp("\\b" + searchTerm + "\\b", "i"))
+			)
+		);
+	prepSearchElements(titleMatches.flat());
 };
 
-btnAddMovie.addEventListener('click', addMovieHandler);
-btnSearch.addEventListener('click', searchMovieHandler);
+const saveMovieToMap = (title, key, value) => {
+	movies.set(title, { [key]: value });
+	createAppendListElements(
+		movieList,
+		title,
+		Object.entries(movies.get(title)).flat()
+	);
+};
+
+const validateSearchInput = () => (searchInput.value.trim() ? true : false);
+
+const validateMovieInputs = () => {
+	// add logic to validate keyName and value
+	const movieTitle = movieInputs[0].value.trim();
+	const key = movieInputs[1].value.trim() || "Extra Info";
+	const value = movieInputs[2].value.trim() || "None!";
+	movieTitle
+		? movies.has(movieTitle)
+			? alert("A movies with that title already exists.")
+			: saveMovieToMap(movieTitle, key, value)
+		: alert("Invalid movie title entered.");
+};
+
+const getSearchTerms = () => searchInput.value.split(" ");
+
+const startSearch = () => searchMovieTitles(getSearchTerms());
+
+const addMovieHandler = () => validateMovieInputs();
+
+const searchMovieHandler = () =>
+	validateSearchInput() ? startSearch() : alert("Invalid search input");
+
+btnAddMovie.addEventListener("click", addMovieHandler);
+btnSearch.addEventListener("click", searchMovieHandler);

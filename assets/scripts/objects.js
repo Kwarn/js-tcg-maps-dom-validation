74607c3d926d46b64b8c6d9a38119644ccaa7766
Kwarn <karlwarner.dev@gmail.com>
2020-05-29 11:00:49 +0100

    Attempting to fix tabs/spaces after switching to Ubuntu

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -3,7 +3,7 @@
 
 // to-do:
 // toggle movie/search list functionality
-// bug - same search elements can appear twice, 
+// bug - same search elements can appear twice,
 // if both search terms are in the same title -consider array.from(new Set())
 
 const movieList = document.getElementById("movie-list");
@@ -11,8 +11,8 @@ const searchList = document.getElementById("search-list");
 const btnAddMovie = document.getElementById("btn-add-movie");
 const btnSearch = document.getElementById("btn-search");
 const movieInputs = document
-	.getElementById("user-input")
-	.querySelectorAll("input");
+  .getElementById("user-input")
+  .querySelectorAll("input");
 const searchInput = document.getElementById("filter-title");
 const movies = new Map();
 
@@ -21,66 +21,68 @@ const hideList = (targetElement) => targetElement.classList.remove("visible");
 const toggleList = () => {};
 
 const clearSearchResults = () => {
-	while (searchList.lastChild) {
-		searchList.removeChild(searchList.lastChild);
-	}
+  while (searchList.lastChild) {
+    searchList.removeChild(searchList.lastChild);
+  }
 };
 
 const createAppendListElements = (targetList, title, extraInfoArr) => {
-	const [extraInfoKey, extraInfoValue] = extraInfoArr;
-	const li = document.createElement("li");
-	li.innerHTML = `
+  const [extraInfoKey, extraInfoValue] = extraInfoArr;
+  const li = document.createElement("li");
+  li.innerHTML = `
   <div>
   <h2>${title}</h2> 
   <p>${extraInfoKey}: ${extraInfoValue}</p>
   </div>
   `;
-	targetList === movieList ? movieList.appendChild(li) : searchList.appendChild(li);
+  targetList === movieList
+    ? movieList.appendChild(li)
+    : searchList.appendChild(li);
 };
 
 const prepSearchElements = (titleMatches) => {
-	for (const title of titleMatches) {
-		createAppendListElements(
-			searchList,
-			title,
-			Object.entries(movies.get(title)).flat()
-		);
-	}
+  for (const title of titleMatches) {
+    createAppendListElements(
+      searchList,
+      title,
+      Object.entries(movies.get(title)).flat()
+    );
+  }
 };
 
 // uses Regex to search map.keys() (movie titles) for exact search term
 const searchMovieTitles = (searchTerms) => {
-	const titleMatches = [];
-	for (const searchTerm of searchTerms)
-		titleMatches.push(
-			Array.from(movies.keys()).filter((key) =>
-				key.match(new RegExp("\\b" + searchTerm + "\\b", "i"))
-			)
-		);
-	prepSearchElements(titleMatches.flat());
+  const titleMatches = [];
+  for (const searchTerm of searchTerms)
+    titleMatches.push(
+      Array.from(movies.keys()).filter((key) =>
+        key.match(new RegExp("\\b" + searchTerm + "\\b", "i"))
+      )
+    );
+  prepSearchElements(titleMatches.flat());
 };
 
 const saveMovieToMap = (title, key, value) => {
-	movies.set(title, { [key]: value });
-	createAppendListElements(
-		movieList,
-		title,
-		Object.entries(movies.get(title)).flat()
-	);
+  movies.set(title, { [key]: value });
+  createAppendListElements(
+    movieList,
+    title,
+    Object.entries(movies.get(title)).flat()
+  );
 };
 
 const validateSearchInput = () => (searchInput.value.trim() ? true : false);
 
 const validateMovieInputs = () => {
-	// add logic to validate keyName and value
-	const movieTitle = movieInputs[0].value.trim();
-	const key = movieInputs[1].value.trim() || "Extra Info";
-	const value = movieInputs[2].value.trim() || "None!";
-	movieTitle
-		? movies.has(movieTitle)
-			? alert("A movies with that title already exists.")
-			: saveMovieToMap(movieTitle, key, value)
-		: alert("Invalid movie title entered.");
+  // add logic to validate keyName and value
+  const movieTitle = movieInputs[0].value.trim();
+  const key = movieInputs[1].value.trim() || "Extra Info";
+  const value = movieInputs[2].value.trim() || "None!";
+  movieTitle
+    ? movies.has(movieTitle)
+      ? alert("A movies with that title already exists.")
+      : saveMovieToMap(movieTitle, key, value)
+    : alert("Invalid movie title entered.");
 };
 
 const getSearchTerms = () => searchInput.value.split(" ");
@@ -90,7 +92,7 @@ const startSearch = () => searchMovieTitles(getSearchTerms());
 const addMovieHandler = () => validateMovieInputs();
 
 const searchMovieHandler = () =>
-	validateSearchInput() ? startSearch() : alert("Invalid search input");
+  validateSearchInput() ? startSearch() : alert("Invalid search input");
 
 btnAddMovie.addEventListener("click", addMovieHandler);
 btnSearch.addEventListener("click", searchMovieHandler);
